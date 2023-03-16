b0f0902144b11ed782af26da5a1d427f9dbf49c0
Jaffenheimer <74659222+Jaffenheimer@users.noreply.github.com>
2023-03-16 11:15:07 +0100

    Update set.js

--- src/commands/set.js
+++ src/commands/set.js
@@ -6,8 +6,8 @@ const rc = (x, y, z, yaw) => commander.send(`rc ${x} ${y} ${z} ${yaw}`);
 
 const wifi = (ssid, password) => commander.send(`wifi ${ssid} ${password}`);
 
-const mon = () => commander.send("mon");
+const mon = () => commander.send("motoron");
 
-const moff = () => commander.send("moff");
+const moff = () => commander.send("motoroff");
 
 module.exports = { speed, rc, wifi, mon, moff };
